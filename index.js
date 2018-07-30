d1bc4e7f13bffd6e386ef42f859f90cbd2df955e
cjoannes@tulane.edu <cjoannes@tulane.edu>
2018-07-30 17:08:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 function theBeatlesPlay(musicians, instruments) { var array = []; for (var i= 0; i < musicians.length; i++) { array.push(musicians[i] + ' plays ' + instruments[i]); } return array }
-
 function johnLennonFacts() { var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]; var array = []; var i = 0; while (array.length < facts.length) { array.push(facts[i] + "!!!"); i++; } return array; }
 function iLoveTheBeatles(num) { var array = []; do { array.push("I love the Beatles!"); num+= 1; } while (num < 15); return array; }
